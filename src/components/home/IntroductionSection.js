4384f638662bc5fcd32457ad3e5a80dd52169a77
Nick Lewis <nickeblewis@gmail.com>
2017-11-28 12:15:46 +0000

    re-written this section

--- src/components/home/IntroductionSection.js
+++ src/components/home/IntroductionSection.js
@@ -45,22 +45,12 @@ class TitleSection extends Component {
         <Container>
           <Row>
             <IntroductionItem
-              title="Flexible"
-              subTitle="Build encapsulated handlers that manage your business logic, then compose them to make complex Bots. Handlers are just JavaScript function which can achieve whatever you want."
+              title="DaaS"
+              subTitle="We have coined (possibly) a new term 'Developer as a Service' or DaaS for short. What this means is you pay us for the time we spend working on your solution and we serve numerous clients."
             />
             <IntroductionItem
-              title="Modern"
-              subTitle="Bottender is written with ES6/ES7 up syntax and it fully supports async await function. We can deal with complex async logic without losing the readability and maintainability of code."
-            />
-          </Row>
-          <Row>
-            <IntroductionItem
-              title="Modular"
-              subTitle="There is a set of interface that defines the core modules provided by Bottender. You can use your own implement of session store, your HTTP server framework or even connect your bot to any platform."
-            />
-            <IntroductionItem
-              title="Learn Once, Write Anywhere"
-              subTitle="Handle multiple platforms with consistent development experience. You can develop bots for different platforms including Messenger, LINE, Slack and Telegram comfortably, with similar mindset."
+              title="Services"
+              subTitle="Our services revolve around a few key areas, such as API based CMS systems and other integrations between the tools that you use for your business. We want to help you make your processes more efficient."
             />
           </Row>
         </Container>
