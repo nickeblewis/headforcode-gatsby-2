bb0809b060110f2bc1cf65bb794587f010f2a149
Nick Lewis <nickeblewis@gmail.com>
2018-01-12 10:06:19 +0000

    Changed subtitle text

--- src/components/home/IntroductionSection.js
+++ src/components/home/IntroductionSection.js
@@ -45,23 +45,23 @@ class TitleSection extends Component {
         <Container>
           <Row>
             <IntroductionItem
-              bold={true}
+              bold={false}
               title="Teaching"
               subTitle="We are learning all of the time and we love to share our experiences with the development community. We plan to release a mixture of FREE and premium courses in the months to come."
             />
             <IntroductionItem
-              bold={true}
+              bold={false}
               title="Consultancy"
               subTitle="We love to solve problems for other businesses through the use of smart technology. We are advocates for the mobile web and ensuring that we all have quick and easy access to information wherever and whenever we need it."
             />
             <IntroductionItem
-              bold={true}
+              bold={false}
               title="Solutions"
               subTitle="We are not fans of re-inventing the wheel completely each time we start a new project. So during quieter periods we keep busy. We are gradually building our own tools and web components for use in future client projects."
             />
             <IntroductionItem
-              bold={true}
-              title="Find out more..."
+              bold={false}
+              title="What next?"
               subTitle="We will be investing more time in our website this year to make sure it's packed with interesting information on coding, building digital businesses and other inspirational material. What's more, we are going to be focussing on the mobile experience, so that headforcode.com becomes your pocket guide to app development!"
             />
           </Row>
