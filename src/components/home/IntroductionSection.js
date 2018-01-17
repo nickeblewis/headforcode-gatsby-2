5aaae9d2dadf9afd34ad234ab01859868a8d532a
Nick Lewis <nickeblewis@gmail.com>
2018-01-17 13:38:17 +0000

    re-worded home page a little bit

--- src/components/home/IntroductionSection.js
+++ src/components/home/IntroductionSection.js
@@ -47,22 +47,22 @@ class TitleSection extends Component {
             <IntroductionItem
               bold={false}
               title="Teaching"
-              subTitle="We are learning all of the time and we love to share our experiences with the development community. We plan to release a mixture of FREE and premium courses in the months to come."
+              subTitle="We are learning all of the time and we love to share our experiences with clients and the development community. We can provide bespoke training to help developers at your business up-skill."
             />
             <IntroductionItem
               bold={false}
               title="Consultancy"
-              subTitle="We love to solve problems for other businesses through the use of smart technology. We are advocates for the mobile web and ensuring that we all have quick and easy access to information wherever and whenever we need it."
+              subTitle="Have you ever considered augmenting your business with a developer who can provide frequent solutions for it? We work remotely and you can reach us during normal working hours via popular telecomms technology (Skype, Hangouts etc)."
             />
             <IntroductionItem
               bold={false}
               title="Solutions"
-              subTitle="We are not fans of re-inventing the wheel completely each time we start a new project. So during quieter periods we keep busy. We are gradually building our own tools and web components for use in future client projects."
+              subTitle="We have a wealth of experience working with all kinds of business across a wealth of marketplaces. So are just as happy to take over on existing projects as we are starting from scratch."
             />
             <IntroductionItem
               bold={false}
               title="What next?"
-              subTitle="We will be investing more time in our website this year to make sure it's packed with interesting information on coding, building digital businesses and other inspirational material. What's more, we are going to be focussing on the mobile experience, so that headforcode.com becomes your pocket guide to app development!"
+              subTitle="We are currently spending time on building this website further so that we can provide a wide range of training resources online, so that you can learn new skills at your own pace."
             />
           </Row>
         </Container>
